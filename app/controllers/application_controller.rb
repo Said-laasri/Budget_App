5c1e9f875eb4802e055bd8a55eced374588cb634
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-11-14 16:26:27 +0100

    Set routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   
     # # Catch all CanCan errors and alert the user of the exception
     def after_sign_out_path_for(_resource_or_scope)
-      '/users/sign_in'
+      '/users/sign_up'
     end
 end
